17ebdad48c7a76784e6a52036e985b9dfb5aae54
vatsalstar <74692621+vatsalstar@users.noreply.github.com>
2021-02-03 07:27:41 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,19 @@
+class Paper{
+constructor(x,y,radius){
+    var options ={
+        isSstatic:false,
+        restritution:0.3,
+        friction:0.05,
+        density:1.2
+    }
+
+
+  function KeyPressed (){
+if(KeyCode=== UP_ARROW){
+
+ matter.body.applyForce(PaperObject.body,papaerObject.body.position,{x:85,y:-85})
+
+}
+}
+}
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C-24
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
